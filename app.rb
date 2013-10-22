de3ac5cffe21c47c05275a7e420f9ccfbbd29244
Daniel Trostli <daniel.trostli@gmail.com>
2013-10-21 22:28:04 -0700

    MAde basic user info displah plus login

--- app.rb
+++ app.rb
@@ -1,13 +1,40 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'dotenv'
+require 'soundcloud'
 
 Dotenv.load(".env")
 
-set :database, ENV['DATABASE_URL'] || 'postgres://localhost/App'
-
+set :database, 'sqlite:///dev.db'
 enable :sessions
 
+
 get '/' do
-  erb :index
+  if session[:user_token]
+    @client = Soundcloud.new(:access_token => session[:user_token])
+    @tracks= @client.get('/me/tracks')
+    p @following = @client.get('/me/followings')
+
+    erb :index
+  else
+    erb :login
+  end
+end
+
+get '/login' do
+  # create client object with app credentials
+  client = Soundcloud.new(:client_id => ENV['SOUNDCLOUD_ID'],
+                        :client_secret => ENV['SOUNDCLOUD_SECRET'],
+                        :redirect_uri => 'http://localhost:9393/auth')
+
+  redirect client.authorize_url()
+end
+
+get '/auth' do
+  client = Soundcloud.new(:client_id => ENV['SOUNDCLOUD_ID'],
+                        :client_secret => ENV['SOUNDCLOUD_SECRET'],
+                        :redirect_uri => 'http://localhost:9393/auth')
+  access_token = client.exchange_token(:code => params[:code]).access_token
+  session[:user_token] = access_token
+  redirect '/'
 end
