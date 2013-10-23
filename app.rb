f9f9249f3bd099106a85f33e4ee5742c8db13b1d
Apprentice <apprentice@dbc10.local>
2013-10-23 12:23:00 -0700

    helper methods simplify routes

--- app.rb
+++ app.rb
@@ -2,35 +2,28 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'dotenv'
 require 'soundcloud'
+require_relative 'helpers/soundcloud_helper'
 
 Dotenv.load(".env")
 
 set :database, 'sqlite:///dev.db'
 enable :sessions
 
-def logged_in?
-  session[:user_token] ? true : false
-end
 
-def active_user
-  @client ||= Soundcloud.new(:access_token => session[:user_token])
-end
 
-def user_profile_info(user)
-  @user_tracks= user.get('/me/tracks')
-  @following_others = user.get('/me/followings')
-end
+helpers do
 
-def client_creator
-  new_client = Soundcloud.new(:client_id => ENV['SOUNDCLOUD_ID'],
-                        :client_secret => ENV['SOUNDCLOUD_SECRET'],
-                        :redirect_uri => 'http://localhost:9393/auth')
+include SoundCloudHelper
+
+  def logged_in?
+      session[:user_token]
+  end
+
 end
 
 
 get '/' do
   if logged_in?
-    user_profile_info(active_user)
     erb :index
   else
     erb :login
@@ -44,7 +37,7 @@ get '/login' do
 end
 
 get '/auth' do
-  session[:user_token] = client_creator.exchange_token(:code => params[:code]).access_token
+  client_access_token(params[:code])
   redirect '/'
 end
 
