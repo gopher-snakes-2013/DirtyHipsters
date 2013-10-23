bc3cbe875a7b146842052bfbe2c9ff30fba583f1
bbeaird <apprentice@dbc18.local>
2013-10-23 11:07:29 -0700

    extracted helper methods, tweaked style, made suggestions in comments

--- app.rb
+++ app.rb
@@ -5,7 +5,9 @@ require 'soundcloud'
 
 Dotenv.load(".env")
 
-set :database, 'sqlite:///dev.db'
+LOCAL_DATABASE_LOCATION = 'sqlite:///dev.db'
+
+set :database, LOCAL_DATABASE_LOCATION
 enable :sessions
 
 def client_creator
@@ -14,12 +16,24 @@ def client_creator
                         :redirect_uri => 'http://localhost:9393/auth')
 end
 
+def set_active_user(token)
+  session[:user_token] = token
+end
+
+def logged_in?
+  session[:user_token] ? true : false
+end
+
+def cached_user_token
+  session[:user_token]
+end
+
 get '/' do
-  if session[:user_token]
-    @client = Soundcloud.new(:access_token => session[:user_token])
+  if logged_in?
+    @client = Soundcloud.new(:access_token => cached_user_token)
     @tracks= @client.get('/me/tracks')
     @following = @client.get('/me/followings')
-
+    @username = @client.get('/me').username
     erb :index
   else
     erb :login
@@ -33,17 +47,17 @@ get '/login' do
 end
 
 get '/auth' do
+# Should this be a post request instead?
   client = client_creator
+  # Do you need to create client in both routes?
   access_token = client.exchange_token(:code => params[:code]).access_token
-  session[:user_token] = access_token
+  set_active_user(access_token)
   redirect '/'
 end
 
-get '/soundcloud'do
-  redirect "http://www.soundcloud.com"
-end
 
 get '/logout' do
   session.clear
+  # why not redirect to your app?
   redirect "http://soundcloud.com"
 end
